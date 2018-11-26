b370329613a4e9a4169a3de07c9f209f5b768608
Richard van den Broek <vandenbroek.rp@gmail.com>
2018-11-26 19:03:07 -0200

    added model validations and made a migration on profile for t.references user

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,14 @@
 class Booking < ApplicationRecord
   belongs_to :listing
   belongs_to :user
+
+  validates :end_after_start
+  validates :start_date, :end_date, :total_cost presence: true
+  validates :guest_number, presence: true, numericality: { greater_than: 0 }
+
+private
+
+  def end_after_start
+    errors.add(:end_date, "must be after the start date") if :end_date < :start_date
+  end
 end
