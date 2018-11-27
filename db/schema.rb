8d03e57fdf1c5e56dacd0175fa3942c6b89aef65
Richard van den Broek <vandenbroek.rp@gmail.com>
2018-11-27 15:45:51 -0200

    created view folders for listings and bookings, created listing_card and index view. added migration for addheadlinetolistings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_202058) do
+ActiveRecord::Schema.define(version: 2018_11_27_173512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2018_11_26_202058) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "headline"
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
