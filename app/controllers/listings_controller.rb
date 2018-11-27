c3b77c28887b0739adee0f188e291094030a4fbd
Manuel Winzler <manu_elco@hotmail.de>
2018-11-27 11:48:28 -0200

    listings controller added

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,26 @@
 class ListingsController < ApplicationController
+   def index
+    @listings = Listings.all
+  end
+
+  def show
+    @listing = Listing.find(params[:id])
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+    @listing.save!
+  end
+
+
+private
+
+  def listing_params
+    params.require(:listing).permit(:description, :country, :city, :address, :price_daily, :capacity)
+  end
 end
+
